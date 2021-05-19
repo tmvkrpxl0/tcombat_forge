1d1cff5de773ad7e492ab007c53e240aad5e61c8
BuildTools <unconfigured@null.spigotmc.org>
2021-05-19 15:40:52 +0900

    Add test key function(will be deleted later), fixed set target mode key

--- src/main/kotlin/com/tmvkrpxl0/tcombat/client/key/KeyHandler.kt
+++ src/main/kotlin/com/tmvkrpxl0/tcombat/client/key/KeyHandler.kt
@@ -1,18 +1,28 @@
 package com.tmvkrpxl0.tcombat.client.key
 
+import com.tmvkrpxl0.tcombat.TCombatMain
+import com.tmvkrpxl0.tcombat.common.capability.capabilities.TargetCapability
+import com.tmvkrpxl0.tcombat.common.network.packets.CBSizeRequestPacket
 import com.tmvkrpxl0.tcombat.common.network.packets.SkillRequestPacket
 import com.tmvkrpxl0.tcombat.common.network.packets.TCombatPacketHandler
 import com.tmvkrpxl0.tcombat.common.network.packets.TargetRequestPacket
 import com.tmvkrpxl0.tcombat.common.skills.ReflectionBlast
 import com.tmvkrpxl0.tcombat.common.skills.RicochetArrow
+import com.tmvkrpxl0.tcombat.common.skills.TestSkill
+import com.tmvkrpxl0.tcombat.common.util.TCombatUtil
+import net.minecraft.block.Blocks
 import net.minecraft.client.Minecraft
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 import net.minecraft.client.settings.KeyBinding
 import net.minecraft.client.util.InputMappings
 import net.minecraftforge.client.event.InputEvent.KeyInputEvent
+import net.minecraftforge.client.model.pipeline.LightUtil
 import net.minecraftforge.client.settings.KeyConflictContext
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.client.registry.ClientRegistry
 import org.lwjgl.glfw.GLFW
+import kotlin.math.max
+import kotlin.math.min
 
 class KeyHandler private constructor(): AbstractKeyHandler(BINDINGS) {
     companion object {
@@ -23,13 +33,15 @@ class KeyHandler private constructor(): AbstractKeyHandler(BINDINGS) {
         val KB_SET_TARGETS = KeyBinding("Set targets", KeyConflictContext.IN_GAME, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_X, CATEGORY)
         val KB_SET_TARGET_MODE = KeyBinding("Set targetting mode", KeyConflictContext.IN_GAME, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_R, CATEGORY)
         val KB_REFLECTION_BLAST = KeyBinding("Reflection blast", KeyConflictContext.IN_GAME, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_N, CATEGORY)
+        val KB_TEST = KeyBinding("Test", KeyConflictContext.IN_GAME, InputMappings.Type.KEYSYM, GLFW.GLFW_KEY_MINUS, CATEGORY)
         val BINDINGS = Builder(10)
             .addBinding(KB_REFLECT_ARROW, false)
             .addBinding(KB_SET_TARGETS, false)
             .addBinding(KB_SET_TARGET_MODE, false)
             .addBinding(KB_REFLECTION_BLAST, false)
+            .addBinding(KB_TEST, false)
         val INSTANCE = KeyHandler()
-        var requestType = TargetRequestPacket.RequestType.PICK_CLOSE
+        var requestType = TargetCapability.RequestType.NEXT_PICK_MODE
     }
 
     fun register() {
@@ -37,6 +49,7 @@ class KeyHandler private constructor(): AbstractKeyHandler(BINDINGS) {
         ClientRegistry.registerKeyBinding(KB_SET_TARGETS)
         ClientRegistry.registerKeyBinding(KB_SET_TARGET_MODE)
         ClientRegistry.registerKeyBinding(KB_REFLECTION_BLAST)
+        ClientRegistry.registerKeyBinding(KB_TEST)
         MinecraftForge.EVENT_BUS.addListener { _: KeyInputEvent -> onKeyInput() }
     }
 
@@ -54,10 +67,15 @@ class KeyHandler private constructor(): AbstractKeyHandler(BINDINGS) {
             }
             kb === KB_SET_TARGET_MODE -> {
                 var ordinal = requestType.ordinal + 1
-                ordinal %= (TargetRequestPacket.RequestType.values().size - 1)
+                ordinal %= (TargetCapability.RequestType.values().size - 1)
+                requestType = TargetCapability.RequestType.values()[ordinal]
             }
             kb === KB_REFLECTION_BLAST -> {
                 TCombatPacketHandler.INSTANCE.sendToServer(SkillRequestPacket(ReflectionBlast))
+            }
+            kb === KB_TEST -> {
+                val skillRequest = SkillRequestPacket(TestSkill)
+                TCombatPacketHandler.INSTANCE.sendToServer(skillRequest)
             }
         }
     }
