28e1afa3de296f7e91a173d259f9b4b061c433ec
BuildTools <unconfigured@null.spigotmc.org>
2021-05-19 15:42:51 +0900

    Forgot to update ITargetHolder

--- src/main/kotlin/com/tmvkrpxl0/tcombat/common/capability/factories/ITargetHolder.kt
+++ src/main/kotlin/com/tmvkrpxl0/tcombat/common/capability/factories/ITargetHolder.kt
@@ -1,11 +1,20 @@
 package com.tmvkrpxl0.tcombat.common.capability.factories
 
+import com.tmvkrpxl0.tcombat.common.capability.capabilities.TargetCapability
 import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.player.PlayerEntity
 
+//On server side, Every player instance should have capability that gets removed when player dies
+//On client side, only `Minecraft.getInstance().getPlayer()` instance should have capability that gets removed when the player dies
 interface ITargetHolder {
     fun getTargets():List<LivingEntity>
     fun getFocused():LivingEntity?
     fun setTargets(targets: List<LivingEntity>)
-    fun setFocused(focused: LivingEntity)
+    fun setFocused(focused: LivingEntity?)
     fun clearTargets()
+    fun getPickMode(): TargetCapability.PickMode
+    fun setPickMode(mode: TargetCapability.PickMode)
+    fun update()
+    fun getPlayer(): PlayerEntity
+    fun setPlayer(player: PlayerEntity)
 }
